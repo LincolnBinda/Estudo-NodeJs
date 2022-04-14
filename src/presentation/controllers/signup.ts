9d85a4e6e4f0316b0e476d3c38b6f5597d1bd7b7
Lincoln Binda <lincoln.binda.silva@gmail.com>
2022-04-13 21:47:37 -0300

    feat: ensure SignUpController returns 400 if no password is provaded

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { badRequest } from '../helpers/http-helpers'
 
 export class SignUpController {
   handler (httpRequest: HttpRequest): HttpResponse {
-    const requireFields = ['name', 'email']
+    const requireFields = ['name', 'email', 'password']
     for (const field of requireFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
